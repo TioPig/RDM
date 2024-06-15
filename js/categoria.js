060c472fcc359d14eab95a0101937ba62c931e1b
TioPig <piggamer1324@gmail.com>
2024-06-14 21:46:28 -0400

    Modal Fixes

--- js/categoria.js
+++ js/categoria.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function() {
                             </div>
 
                             <!-- Modal -->
-                            <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="${modalId}Label" aria-hidden="true">
+                            <div class="modal fade modal-lg" style="text-align: justify !important;" id="${modalId}" tabindex="-1" aria-labelledby="${modalId}Label" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
--- js/paises.js
+++ js/paises.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
                             </div>
 
                             <!-- Modal -->
-                            <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="${modalId}Label" aria-hidden="true">
+                            <div class="modal fade modal-lg" style="text-align: justify !important;" id="${modalId}" tabindex="-1" aria-labelledby="${modalId}Label" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
